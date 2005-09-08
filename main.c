a71e9468ec7dc7247598be3cd01a113200273dc0
peter <peter@50aeb178-8500-dd11-b8ad-001a4d4a7278>
2005-09-08 14:25:07 +0000

    пикаем, но что-то тихо

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
  #include  <msp430x11x1.h>
-
+ #include "includes.h"
 void main(void)
 { 
   WDTCTL = WDTPW + WDTHOLD;             // Stop watchdog timer
   P2DIR |= 0x03;                        // Set P2.0, P2.1 to output direction
-
+  P1DIR |= BIT2|BIT3;
+  P1OUT &= ~BIT2;
+  P1OUT |= BIT3;
+  init_params();
   volatile unsigned int y;
 
   for (;;)                              
@@ -27,8 +30,10 @@ void main(void)
       break;
      }
 
-    i = 10000;                          // Delay
-    do (i--);
-    while (i != 0);
+    i = 15000;                          // Delay
+    do{ i--;
+    if ((i<1000)&&((i&0x3)==0)) 
+      P1OUT^=(BIT2|BIT3);
+    } while (i != 0);
   }
 }
